7220e9b31d2a04a7d59598e4230bc7a78956a8ae
Ryan LeBlanc <ryanleb2003@gmail.com>
2023-10-20 10:35:38 -0300

    Working!

--- HealthSystem-Fix/HealthSystem-Fix/Program.cs
+++ HealthSystem-Fix/HealthSystem-Fix/Program.cs
@@ -1,342 +1,308 @@
 ﻿using System;
 using System.Diagnostics;
 
-class Player
+namespace HealthSystem
 {
-    // creating variables
-
-    private int maxHealth = 100;
-    private int health;
-    private int maxShield = 100;
-    private int currentShield;
-    private int xp;
-    private int level;
-    private int lives;
-
-    
-    // defining player
-    
-    public Player(int maxHealth, int maxShield, int startingLives)
+    internal class Program
     {
-        this.maxHealth = maxHealth;
-        this.health = maxHealth;
-        this.maxShield = maxShield;
-        this.currentShield = maxShield;
-        this.xp = 0;
-        this.level = 1;
-        this.lives = startingLives;
-    }
+        // creating variables
+
+        static int maxHealth = 100;
+        static int health;
+        static int maxShield = 100;
+        static int shield;
+        static int xp;
+        static int level;
+        static int lives;
 
 
-    // take damage method
-
-    public void TakeDamage(int damage)
-    {
-        if (damage < 0)
-            
-            damage = 0;
-            
         
-        
-        
-        if (currentShield > 0)
+        static void Main(string[] args)
         {
-            int remainingDamage = damage - currentShield;
-            if (remainingDamage > 0)
+
+
+            Console.WriteLine("Welcome to a health system simulator!");
+            Console.WriteLine();
+            Console.WriteLine("Made by RyanDot Games");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit!");
+            Console.WriteLine();
+
+            health = 100;
+            shield = 100;
+            level = 1;
+
+
+
+
+
+            ShowHUD();
+
+
+            Console.WriteLine("You take 30 damage!");
+            Console.WriteLine();
+
+            TakeDamage(30);
+            ShowHUD();
+
+
+            Console.WriteLine("You've activated a buff which regenerates 20 shield!");
+            Console.WriteLine();
+            RechargeShield(20);
+            ShowHUD();
+
+            Console.WriteLine("You take 100 damage!");
+            Console.WriteLine();
+            TakeDamage(100);
+            ShowHUD();
+
+
+            Console.WriteLine("You defeat the monster and gain 150 experience!");
+            Console.WriteLine();
+            Console.WriteLine("You leveled up! You are now level 2!");
+            Console.WriteLine();
+            IncreaseXP(150);
+            ShowHUD();
+
+
+            Console.WriteLine("You drink a potion and heal 10 hp");
+            Heal(10);
+            ShowHUD();
+
+            Console.WriteLine("You take a rest and regenerate your shield");
+            RechargeShield(100);
+            ShowHUD();
+
+            Console.WriteLine("You encounter another monster!");
+            Console.WriteLine();
+            ShowHUD();
+
+            Console.WriteLine("You take 200 damage!");
+            TakeDamage(200);
+            ShowHUD();
+
+            Console.WriteLine("You have died, you will revive in a safe location");
+            Revive();
+            Console.WriteLine();
+            ShowHUD();
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+
+
+
+        // take damage method
+
+        static void TakeDamage(int damage)
+        {
+            if (damage < 0)
+
+                damage = 0;
+
+
+
+
+            if (shield > 0)
             {
-                currentShield = 0;
-                health -= remainingDamage;
+                int remainingDamage = damage - shield;
+                if (remainingDamage > 0)
+                {
+                    shield = 0;
+                    health -= remainingDamage;
+                }
+                else
+                {
+                    shield -= damage;
+                }
             }
             else
             {
-                currentShield -= damage;
+                health -= damage;
+            }
+            if (health < 0)
+            {
+                if (lives > 0)
+                {
+                    lives--;
+                    health = maxHealth;
+                    shield = maxShield;
+                }
+                else
+                {
+                    health = 0;
+                }
             }
         }
-        else
+
+
+
+
+        // healing and shield recharge method
+        static void Heal(int healAmount)
         {
-            health -= damage;
+            if (healAmount < 0)
+
+                healAmount = 0;
+
+
+
+            health += healAmount;
+            if (health > maxHealth)
+            {
+                health = maxHealth;
+            }
+
         }
-        if (health < 0)
+
+        static void RechargeShield(int rechargeAmount)
+        {
+            if (rechargeAmount < 0)
+
+                rechargeAmount = 0;
+
+            shield += rechargeAmount;
+            if (shield > maxShield)
+            {
+
+                shield = maxShield;
+            }
+        }
+
+        // "Get" methods
+
+        static int GetCurrentShield()
+        {
+            return shield;
+
+        }
+        static int GetMaxShield()
+        {
+            if (maxShield > 100)
+            {
+                Console.WriteLine("You cannot exceed 100 shield!");
+                maxShield = 100;
+            }
+            return maxShield;
+        }
+        static int GetExperienceRequiredForNextLevel()
+        {
+            return level * 100;
+        }
+
+
+        // Increase XP method
+
+        static void IncreaseXP(int expAmount)
+        {
+            if (expAmount < 0)
+
+                return;
+
+
+
+
+            xp += expAmount;
+
+            while (xp >= GetExperienceRequiredForNextLevel())
+            {
+                xp -= GetExperienceRequiredForNextLevel();
+                level++;
+            }
+
+
+
+        }
+
+        // methods to get current and maximum health available
+        static int GetCurrentHealth()
+        {
+            return health;
+
+
+        }
+
+        // Get methods
+
+        static int GetMaxHealth()
+        {
+            if (maxHealth > 100)
+            {
+                Console.WriteLine("You cannot exceed 100 HP!");
+                maxHealth = 100;
+            }
+            return maxHealth;
+        }
+        static void Revive()
         {
             if (lives > 0)
             {
                 lives--;
-                health = maxHealth;
-                currentShield = maxShield;
-            }
-            else
-            {
-                health = 0;
+                Heal(GetMaxHealth());
+                RechargeShield(GetMaxShield());
             }
         }
-    }
-
-
-
-
-    // healing and shield recharge method
-    public void Heal(int healAmount)
-    {
-        if (healAmount < 0)
-            
-            healAmount = 0;
-        
-        
-        
-        health += healAmount;
-        if (health > maxHealth)
+        static int GetLives()
         {
-            health = maxHealth;
+            return lives;
         }
-        
-    }
 
-    public void RechargeShield(int rechargeAmount)
-    {
-        if (rechargeAmount < 0)
-            
-            rechargeAmount = 0;
-        
-        currentShield += rechargeAmount;
-        if (currentShield > maxShield)
-        { 
-            
-            currentShield = maxShield;
-        }
-    }
-    
-    // "Get" methods
-    
-    public int GetCurrentShield()
-    {
-        return currentShield;
-       
-    }
-    public int GetMaxShield()
-    {
-        if (maxShield > 100)
+        static int GetCurrentLevel()
         {
-            Console.WriteLine("You cannot exceed 100 shield!");
-            maxShield = 100;
+            return level;
         }
-        return maxShield;
-    }
-    public int GetExperienceRequiredForNextLevel()
-    {
-        return level * 100;
-    }
-   
-    
-    // Increase XP method
-    
-    public void IncreaseXP(int expAmount)
-    {
-        if (expAmount < 0)
-            
-            expAmount = 0;
-            
-        
-        
-        
-        xp += expAmount;
-        
-        while (xp >= GetExperienceRequiredForNextLevel())
+
+        static int GetCurrentExperience()
         {
-            xp -= GetExperienceRequiredForNextLevel();
-            level++;
+            return xp;
         }
 
 
+        // ShowHUD method to display in console
 
-    }
-
-    // methods to get current and maximum health available
-    public int GetCurrentHealth()
-    {
-
-        if (health == 100)
+        static void ShowHUD()
         {
-            Console.WriteLine();
-            Console.WriteLine("You are in perfect health!");
-            Console.WriteLine();
-
-        }
-        if (health <= 90)
-        {
-            Console.WriteLine();
-            Console.WriteLine("You are Healthy!");
+            Console.WriteLine("Player Status:");
+            Console.WriteLine("Lives Remaining: " + lives);
+            Console.WriteLine("Health: " + GetCurrentHealth() + " / " + GetMaxHealth());
+            Console.WriteLine("Shield: " + GetCurrentShield() + " / " + GetMaxShield());
+            Console.WriteLine("Level:" + GetCurrentLevel());
+            Console.WriteLine("Experience: " + GetCurrentExperience() + " / " + GetExperienceRequiredForNextLevel());
             Console.WriteLine();
         }
 
-        if (health <= 75)
-        {
-            Console.WriteLine();
-            Console.WriteLine("You are Hurt!");
-            Console.WriteLine();
-        }
 
-        if (health <= 50)
-        {
-            Console.WriteLine();
-            Console.WriteLine("You are badly Hurt! Seek healing!");
-            Console.WriteLine();
-        }
 
-        if (health <= 10)
-        {
-            Console.WriteLine();
-            Console.WriteLine("You are in Imminent Danger!!! Find Assistance!");
-            Console.WriteLine();
 
-        }
+
         
-        return health;
 
     }
-
-    // Get methods
-
-    public int GetMaxHealth()
-    {
-        if (maxHealth > 100)
-        {
-            Console.WriteLine("You cannot exceed 100 HP!");
-            maxHealth = 100;
-        }
-        return maxHealth;
-    }
-    public void Revive()
-    {
-        if (lives > 0)
-        {
-            lives--;
-            Heal(GetMaxHealth());
-            RechargeShield(GetMaxShield());
-        }
-    }
-    public int GetLives()
-    {
-        return lives;
-    }
-
-    public int GetCurrentLevel()
-    {
-        return level;
-    }
-
-    public int GetCurrentExperience()
-    {
-        return xp;
-    }
-
-    
-    // ShowHUD method to display in console
-    
-    public void ShowHUD()
-    {
-        Console.WriteLine("Player Status:");
-        Console.WriteLine("Lives Remaining: " + lives);
-        Console.WriteLine("Health: " + GetCurrentHealth() + " / " + GetMaxHealth());
-        Console.WriteLine("Shield: " + GetCurrentShield() + " / " + GetMaxShield());
-        Console.WriteLine("Level:" + GetCurrentLevel());
-        Console.WriteLine("Experience: " + GetCurrentExperience() + " / " + GetExperienceRequiredForNextLevel());
-        Console.WriteLine();
-    }
-
-
-    
-
 
 }
+
+   
 
 
 // Main 
 
-class Program
-{
+
+
     
     
     
     
     
-    static void Main(string[] args)
-    {
-
-       
-
-
-
-        Console.WriteLine("Welcome to a health system simulator!");
-        Console.WriteLine();
-        Console.WriteLine("Made by RyanDot Games");
-        Console.WriteLine();
-        Console.WriteLine();
-
-
-        Player player = new Player(100, 100, 3);
-
-        player.ShowHUD();
-        
-
-        Console.WriteLine("You take 30 damage!");
-        Console.WriteLine();
-
-        player.TakeDamage(30);
-        player.ShowHUD();
-
-
-        Console.WriteLine("You've activated a buff which regenerates 20 shield!");
-        Console.WriteLine();   
-        player.RechargeShield(20);
-        player.ShowHUD();
-
-        Console.WriteLine("You take 100 damage!");
-        Console.WriteLine();   
-        player.TakeDamage(100);
-        player.ShowHUD();
-
-
-        Console.WriteLine("You defeat the monster and gain 150 experience!") ;
-        Console.WriteLine();
-        Console.WriteLine("You leveled up! You are now level 2!");
-        player.IncreaseXP(-150);
-        player.ShowHUD();
-
-
-        Console.WriteLine("You drink a potion and heal 10 hp");
-        player.Heal(10);
-        player.ShowHUD();
-        
-        Console.WriteLine("You take a rest and regenerate your shield") ;
-        player.RechargeShield(100);
-        player.ShowHUD();
-
-        Console.WriteLine("You encounter another monster!");
-        Console.WriteLine();
-        player.ShowHUD();
-
-        Console.WriteLine("You take 200 damage!");
-        player.TakeDamage(200);
-        player.ShowHUD();
-
-        Console.WriteLine("You have died, you will revive in a safe location");
-        player.Revive();
-        Console.WriteLine();
-        player.ShowHUD();
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        Console.ReadKey();
-    }
-}
+    
     
     
 
